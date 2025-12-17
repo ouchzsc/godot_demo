ecda707b8f422d63a08c6ff292cb8ec695330db4
zhangao <ouchzsc@gmail.com>
2025-12-18 02:33:39 +0800

    loading

--- game/script/base/scene_mgr.gd
+++ game/script/base/scene_mgr.gd
@@ -1,8 +1,14 @@
 extends Node
 class_name SceneMgr
+var isLoading = false
 
 func load(scene_path: String):
+	if isLoading:
+		return
+	isLoading = true
 	var loader = game.res.load(scene_path)
 	await loader.finished
 	var scene: PackedScene = loader.asset
 	get_tree().change_scene_to_packed(scene)
+	await get_tree().scene_changed
+	isLoading = false
